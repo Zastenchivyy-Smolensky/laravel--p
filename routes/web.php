1c1d5dbad38322de6d8087ba02499ca706d8def6
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-04-04 16:15:37 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("/", "App\Http\Controllers\TodolistFormController@index");
+Route::get("/create-page", "App\Http\Controllers\TodolistFormController@createPage");
+Route::post("/create", "App\Http\Controllers\TodolistFormController@create");
+Route::get("/edit-page/{id}","App\Http\Controllers\TodolistFormController@editPage");
+Route::post('/edit', 'App\Http\Controllers\TodolistFormController@edit');
+Route::get('/delete-page/{id}', 'App\Http\Controllers\TodolistFormController@deletePage');
+Route::post('/delete/{id}','App\Http\Controllers\TodolistFormController@delete');
